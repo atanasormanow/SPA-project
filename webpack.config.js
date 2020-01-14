998cd05846d5899d9a401066e041fb9c59397956
Atanas <atanasormanow@gmail.com>
2020-01-14 17:41:31 +0200

    Change webpack port

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.join(__dirname + 'dist'),
-    port: 3000,
+    port: 3210,
     headers: {
         'Access-Control-Allow-Origin': '*',
     }
