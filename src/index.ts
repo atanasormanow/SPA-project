d1a13fd559581ec1f40f54f51add6ca01f4a1b79
Atanas <atanasormanow@gmail.com>
2020-01-15 19:29:18 +0200

    WIP: set pin

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,48 @@
 import { OpenStreetMapProvider } from 'leaflet-geosearch';
 import leaflet, { marker } from 'leaflet';
 import * as data from '../static/data.json';
+import * as locations from '../static/locations.json';
+
+type Data = {
+  operator: string;
+  object: string;
+  location: string;
+  safeRadius: string;
+  IMFProtocol: string;
+  buildAllowance: string;
+};
+
+type Location = {
+  lat: number,
+  lon: number,
+  icon: string,
+}
 
 var map = leaflet.map('mapid').setView([42.698334, 23.318841], 13);
 const provider = new OpenStreetMapProvider();
 
 leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-async function getResult(location: string) {
-    const results = await provider.search({ query: location });
-    if(!results.length){
-        return [0,0];
-    }
-    const lat = Number(results[0].y);
-    const lon = Number(results[0].x);
-    return [lat, lon];
+function main() {
+  let zipped: Array<any> = [];
+  for (let i in data) {
+    zipped.push([data[i], locations[i]]);
+  }
 
-}
+  const filtered: Array<any> =
+    zipped
+      .filter(([d, l]) => l.length)
+      .map(([d, l]) => [d, l[0]]);
 
-async function getCoords(){
-    let coordinates = [];
-    for(let x in data) {
-        coordinates.push(await getResult(data[x].location))
-    }
-    return coordinates;
-}
-
-async function main(){
-    const coordinates = (await getCoords());
-    //don't dos leaflet :(
-    coordinates.forEach(c => leaflet.marker([c[0], c[1]]).addTo(map).bindPopup('asdf').openPopup());
+  // Doesn't work
+  const pin = leaflet.icon({iconUrl: '../static/pin.png'})
+  filtered
+    .forEach(([d, l]: Array<any>) => marker([l.lat, l.lon], {icon: pin})
+      .addTo(map)
+      .bindPopup('asdf')
+      .openPopup());
 }
 
 main();
