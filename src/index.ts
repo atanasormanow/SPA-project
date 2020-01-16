03cd665fab331dfd7c7032e9fad79bbd86d78228
Atanas <atanasormanow@gmail.com>
2020-01-16 21:41:33 +0200

    Remove unused definitions

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,8 @@
-import { OpenStreetMapProvider } from 'leaflet-geosearch';
 import leaflet, { marker } from 'leaflet';
 import * as data from '../static/data.json';
 import * as locations from '../static/locations.json';
 
-type Data = {
-  operator: string;
-  object: string;
-  location: string;
-  safeRadius: string;
-  IMFProtocol: string;
-  buildAllowance: string;
-};
-
-type Location = {
-  lat: number,
-  lon: number,
-  icon: string,
-}
 var map = leaflet.map('mapid').setView([42.698334, 23.318841], 13);
-const provider = new OpenStreetMapProvider();
 
 leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -35,7 +19,6 @@ function main() {
       .filter(([_, l]) => l.length)
       .map(([d, l]) => [d, l[0]]);
 
-  // Doesn't work
   const pin = leaflet.icon({iconUrl: 'https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-2/512/map_marker_base-256.png',
     iconSize:     [50, 50], // size of the icon
   });
