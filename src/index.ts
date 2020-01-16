ebb3e99d39de9a09d5b86dc56ce49a13d91b864d
Atanas <atanasormanow@gmail.com>
2020-01-17 01:26:02 +0200

    Add popup discription and add radiuses

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import leaflet, { marker } from 'leaflet';
+import leaflet, { marker, circle } from 'leaflet';
 import * as data from '../static/data.json';
 import * as locations from '../static/locations.json';
 
@@ -19,14 +19,47 @@ function main() {
       .filter(([_, l]) => l.length)
       .map(([d, l]) => [d, l[0]]);
 
-  const pin = leaflet.icon({iconUrl: 'https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-2/512/map_marker_base-256.png',
-    iconSize:     [50, 50], // size of the icon
+  const pin = leaflet.icon({
+    iconUrl: 'https://cdn2.iconfinder.com/data/icons/color-svg-vector-icons-2/512/map_marker_base-256.png',
+    iconSize: [50, 50], // size of the icon
   });
+
+  const def = (a: string) => a ? a : 'няма данни';
+
+  const getPopupInfo = (
+    operator: string,
+    object: string,
+    location: string,
+    safeRadius: string,
+    buildAllowance: string) => {
+    return `<b>оператор</b>: ${operator}<br>
+            <b>обект</b>: ${object}<br>
+            <b>адрес</b>: ${location}<br>
+            <b>радиус на ЕМП</b>: ${safeRadius}<br>
+            <b>разрешение за строеж</b>: ${buildAllowance}`;
+  }
+
+  const radiusToNum = (r: string) => {
+    const str = +r.split('м')[0];
+    return isNaN(str) ? 0 : str;
+  };
+
   filtered
-    .forEach(([d, l]: Array<any>) => marker([l.lat, l.lon], {icon: pin})
-      .addTo(map)
-      .bindPopup('asdf')
-      .openPopup());
+    .forEach(([d, l]: Array<any>) => {
+      marker([l.lat, l.lon], { icon: pin })
+        .addTo(map)
+        .bindPopup(getPopupInfo(
+          def(d.operator),
+          def(d.object),
+          def(d.location),
+          def(d.safeRadius),
+          def(d.buildAllowance)
+        ));
+      circle([l.lat, l.lon], {
+        radius: radiusToNum(d.safeRadius)
+      }).addTo(map);
+    });
+  return;
 }
 
 main();
